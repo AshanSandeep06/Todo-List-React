d29a4320afdccc4257dcb573fdef68ed036cab5f
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-31 11:26:35 +0530

    Content Updated in  App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Paper, TextField } from "@mui/material";
+import { Button, Paper, TextField } from "@mui/material";
 
 export default function App() {
   return (
     <div className="App w-screen h-screen flex justify-center items-center">
       <Paper id="taskListContainer" className="w-3/4 h-3/4" elevation={6}>
-        <form>
+        <form className="flex items-center flex-col">
           <div>
             <h1>To Do List</h1>
           </div>
 
-          <div>
-            <div className="w-3/4 mb-5">
+          <div className="flex w-3/4">
+            <div className="w-3/4">
               <TextField
                 id="txtAddTaskField"
                 label="Task Bar"
@@ -25,14 +25,15 @@ export default function App() {
               />
             </div>
 
-            <div className="flex gap-8 justify-center w-full">
-              <button
-                id="btnGeneratePassword"
+            <div className="flex gap-8 justify-center">
+              <Button
                 type="button"
-                className="mt-2 bg-blue-900 text-white py-[10px] px-[18px] rounded-lg font-medium"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 1, mb: 1 }}
               >
-                Generate Password
-              </button>
+                Add Task
+              </Button>
             </div>
           </div>
         </form>
