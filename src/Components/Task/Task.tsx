b47b4e2337dca13992ac76839d054236d14ec427
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-31 12:37:52 +0530

    Content Updated in Task Component

--- src/Components/Task/Task.tsx
+++ src/Components/Task/Task.tsx
@@ -4,7 +4,7 @@ import DeleteIcon from "@mui/icons-material/Delete";
 export default function Task() {
   return (
     <section className="task flex w-full px-[60px]">
-      <div className="task-text w-3/4 mr-5">
+      <div className="task-text w-3/4 mr-8">
         <TextField
           id="filled-basic"
           label="Task-01"
@@ -13,7 +13,7 @@ export default function Task() {
         />
       </div>
 
-      <div className="buttons flex gap-5 h-1/2 self-center">
+      <div className="buttons flex gap-4 h-1/2 self-center">
         <Button color="primary" variant="contained">
           Update
         </Button>
