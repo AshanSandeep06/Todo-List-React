f34b09bf2417ba9fafce3cd7d4fcaf9dc9969afd
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-31 10:46:09 +0530

    Added tailwind-css imports for index.css file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Paper } from '@mui/material';
 
 export default function App() {
   return (
-    <div className="App">
-      
+    <div className="App w-screen h-screen">
+      <Paper elevation={3} />
     </div>
   );
 }
