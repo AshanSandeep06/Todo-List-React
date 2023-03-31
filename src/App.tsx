24f8a13eba426f4d3d38bbf475d9dae79bc7aa9e
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-31 11:12:54 +0530

    Content Updated in  App.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -12,26 +12,28 @@ export default function App() {
             <h1>To Do List</h1>
           </div>
 
-          <div className="w-3/4 mb-5">
-            <TextField
-              id="txtAddTaskField"
-              label="Task Bar"
-              type="text"
-              variant="outlined"
-              placeholder="What are your plans today .?"
-              fullWidth={true}
-              autoFocus
-            />
-          </div>
+          <div>
+            <div className="w-3/4 mb-5">
+              <TextField
+                id="txtAddTaskField"
+                label="Task Bar"
+                type="text"
+                variant="outlined"
+                placeholder="What are your plans today .?"
+                fullWidth={true}
+                autoFocus
+              />
+            </div>
 
-          <div className="flex gap-8 justify-center w-full">
-            <button
-              id="btnGeneratePassword"
-              type="button"
-              className="mt-2 bg-blue-900 text-white py-[10px] px-[18px] rounded-lg font-medium"
-            >
-              Generate Password
-            </button>
+            <div className="flex gap-8 justify-center w-full">
+              <button
+                id="btnGeneratePassword"
+                type="button"
+                className="mt-2 bg-blue-900 text-white py-[10px] px-[18px] rounded-lg font-medium"
+              >
+                Generate Password
+              </button>
+            </div>
           </div>
         </form>
       </Paper>
