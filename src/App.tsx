6be44d017708e117bb982c99efc2f1f99684beaa
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-31 15:17:31 +0530

    Content Updated with class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Button, Paper, TextField } from "@mui/material";
@@ -9,78 +9,62 @@ import $ from "jquery";
 import { log } from "console";
 import ReactDOM from "react-dom/client";
 
-export default function App() {
-  const [state, setState] = useState(0);
-
-  // const [value, setValue] = useState("");
-
-  // const [id, setId] = useState(0);
-
-  const [[], setDeTaskDetails] = useState([]);
-
-  useEffect(() => {
-    // WRITE CODE HERE
-
-    $("#btnAddTask").on("click", function () {
-      addTask();
-    });
-
-    function addTask(): void {
-      if ($("#txtAddTaskField").val() !== "") {
-        $("#taskContainer").append("<Task/>");
-      }
-    }
-  }, []);
-
-  return (
-    <div className="App w-screen h-screen flex justify-center items-center">
-      <Paper id="taskListContainer" className="w-3/4 h-3/4" elevation={6}>
-        <form className="flex items-center flex-col px-5 py-4 gap-5">
-          <div className="text-[28px] font-semibold">
-            <h1>
-              To-Do List <AssignmentIcon className="mb-[2px]" />
-            </h1>
-          </div>
-
-          <div className="flex w-full gap-[45px] px-12">
-            <div className="w-3/4">
-              <TextField
-                id="txtAddTaskField"
-                label="Task Bar"
-                type="text"
-                variant="outlined"
-                placeholder="What are your plans today .?"
-                fullWidth={true}
-                autoFocus
-              />
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App w-screen h-screen flex justify-center items-center">
+        <Paper id="taskListContainer" className="w-3/4 h-3/4" elevation={6}>
+          <form className="flex items-center flex-col px-5 py-4 gap-5">
+            <div className="text-[28px] font-semibold">
+              <h1>
+                To-Do List <AssignmentIcon className="mb-[2px]" />
+              </h1>
             </div>
 
-            <div className="flex gap-8 justify-center">
-              <Button
-                id="btnAddTask"
-                type="button"
-                fullWidth
-                color="success"
-                variant="contained"
-                sx={{ mt: 1, mb: 1 }}
-              >
-                Add Task
-              </Button>
+            <div className="flex w-full gap-[45px] px-12">
+              <div className="w-3/4">
+                <TextField
+                  id="txtAddTaskField"
+                  label="Task Bar"
+                  type="text"
+                  variant="outlined"
+                  placeholder="What are your plans today .?"
+                  fullWidth={true}
+                  autoFocus
+                />
+              </div>
+
+              <div className="flex gap-8 justify-center">
+                <Button
+                  id="btnAddTask"
+                  type="button"
+                  fullWidth
+                  color="success"
+                  variant="contained"
+                  sx={{ mt: 1, mb: 1 }}
+                >
+                  Add Task
+                </Button>
+              </div>
             </div>
-          </div>
 
-          <div className="text-[22px] font-semibold self-start mt-2 mb-1">
-            <h1>
-              Today Tasks <TaskAltIcon className="mb-[2px]" />
-            </h1>
-          </div>
+            <div className="text-[22px] font-semibold self-start mt-2 mb-1">
+              <h1>
+                Today Tasks <TaskAltIcon className="mb-[2px]" />
+              </h1>
+            </div>
 
-          <div
-            id="taskContainer"
-            className="w-full px-[60px]n flex flex-col gap-5 overflow-y-auto h-[298px]"
-          ></div>
-        </form>
-      </Paper>
-    </div>
-  );
+            <div
+              id="taskContainer"
+              className="w-full px-[60px]n flex flex-col gap-5 overflow-y-auto h-[298px]"
+            >
+              <Task/>
+              <Task/>
+              <Task/>
+            </div>
+          </form>
+        </Paper>
+      </div>
+    );
+  }
 }
