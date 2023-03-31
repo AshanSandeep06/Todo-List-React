408c10de7afa13cb2fc1c9a089effb39bb62f25a
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-31 10:57:40 +0530

    Content Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Paper } from '@mui/material';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Paper } from "@mui/material";
 
 export default function App() {
   return (
     <div className="App w-screen h-screen flex justify-center items-center">
-      <Paper id="taskListContainer" className='w-1/2 h-1/2' elevation={6} />
+      <Paper id="taskListContainer" className="w-3/4 h-3/4" elevation={6}>
+        <form>
+          <h1>To Do List</h1>
+        </form>
+      </Paper>
     </div>
   );
 }
