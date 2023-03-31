dbf54d6bfb902a979b5c6ac71e03b5a6e6127856
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-31 12:08:43 +0530

    Conetnt Updated in Task Component

--- src/Components/Task/Task.tsx
+++ src/Components/Task/Task.tsx
@@ -1,8 +1,10 @@
+import { TextField } from "@mui/material";
+
 export default function Task() {
   return (
-    <section className="task">
+    <section className="task flex border-2 border-black">
       <div className="task-text">
-        <input type="text" />
+        <TextField id="filled-basic" label="Task-01" variant="filled" />
       </div>
 
       <div className="buttons">
