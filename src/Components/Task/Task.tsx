ac687ef08e7f2fed3f68d5985a914d0545cf2b60
AshanSandeep06 <ashansandeep06@gmail.com>
2023-03-31 12:02:15 +0530

    Conetnt Refactored in Task Component

--- src/Components/Task/Task.tsx
+++ src/Components/Task/Task.tsx
@@ -1,7 +1,14 @@
 export default function Task() {
-    return(
-        <div className="task">
-            
-        </div>
-    );
+  return (
+    <section className="task">
+      <div className="task-text">
+        <input type="text" />
+      </div>
+
+      <div className="buttons">
+        <button>Update</button>
+        <button>Remove</button>
+      </div>
+    </section>
+  );
 }
